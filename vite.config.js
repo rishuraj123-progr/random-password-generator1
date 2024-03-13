eaadd1fb879735aa2d80de53ef0869c568b80a9b
rishuraj123-progr <rishuraj9002@gmail.com>
2024-03-13 06:45:01 -0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"{repName}"
+  base:"/05-password-generator/"
 })
